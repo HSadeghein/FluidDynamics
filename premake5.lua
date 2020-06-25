7776dcd8b106f6162738abf97e3a98c5ec453982
Mohammad Hassan Sadeghein <h.sadeghein96@gmail.com>
2020-06-25 12:19:26 +0300

    premake updated, googltest build option has been changed to /md

--- premake5.lua
+++ premake5.lua
@@ -23,12 +23,15 @@ include "vendors/glad"
 
 -- googltest uses /MT by defualt in the VS project setting == >Causes the application to use the multithread, static version of the run-time library. Defines _MT and causes the compiler to place the library name LIBCMT.lib into the .obj file so that the linker will use LIBCMT.lib to resolve external symbols.
 -- to change that we need to add line in CMAKE file of the googletest solution ==> set( gtest_force_shared_crt ON CACHE BOOL "Always use msvcrt.dll" FORCE) by doing this we make it to use /MD
+-- the upper line will not effect because in the inne cmake file there is a line which makes gtest_force_shared_crt OFF ---> it is on the first line of the cmake file by making it ON we can build our solution.
+-- also we need to define the google test version in the cmake file.
 -- also it needs project to be in Debug mode so we must add runtime "Debug"
 project "sample"
 	location "sample"
 	language "C++"
 	kind "ConsoleApp"
 	runtime "Debug"
+
 
 	targetdir("bin/" .. outputdir .. "/%{prj.name}")
 	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
